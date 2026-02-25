08acf2620864deeca8493c30a06f685ec2d38e28
Havier Andrei Pamfile Nastase <andyhavy@gmail.com>
2026-02-25 22:20:26 +0100

    Fix variable escaping in post-boot commands

--- start-payara-micro.sh
+++ start-payara-micro.sh
@@ -3,16 +3,17 @@ set -eu
 
 echo "=== Generating post-boot commands ==="
 
+# La barra invertida \ delante de $ es VITAL para que las variables 
+# se lean desde Render y no se pierdan al crear el archivo.
 cat > /opt/payara/config/post-boot-generated.asadmin <<EOF
-create-jdbc-connection-pool --datasourceclassname=org.postgresql.ds.PGSimpleDataSource --restype=javax.sql.DataSource --property user=${DB_USER}:password=${DB_PASSWORD}:serverName=${DB_HOST}:portNumber=${DB_PORT}:databaseName=${DB_NAME}:sslmode=${DB_SSL_MODE} PostgresPool
+create-jdbc-connection-pool --datasourceclassname=org.postgresql.ds.PGSimpleDataSource --restype=javax.sql.DataSource --property user=\${DB_USER}:password=\${DB_PASSWORD}:serverName=\${DB_HOST}:portNumber=\${DB_PORT}:databaseName=\${DB_NAME}:sslmode=\${DB_SSL_MODE} PostgresPool
 create-jdbc-resource --connectionpoolid=PostgresPool jdbc/vuelosDS
 EOF
 
 echo "=== Post-boot file contents ==="
 cat /opt/payara/config/post-boot-generated.asadmin
 
-exec java \
-  -jar /opt/payara/payara-micro.jar \
+exec java -jar /opt/payara/payara-micro.jar \
   --noCluster \
   --port 8080 \
   --addlibs /opt/payara/libs/postgresql.jar \
